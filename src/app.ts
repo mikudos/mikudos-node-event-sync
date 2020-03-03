685c4f2c898569697e65884ebf5649314fac22c3
guanyu <yue.guanyu@hotmail.com>
2020-03-03 17:20:19 +0800

    prepare for scene play

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,57 @@
 import config from 'config';
 import _ from 'lodash';
+import Mali from 'mali';
 
-export class Application {}
+export declare namespace mikudos {
+    interface ConfigFunc {
+        (app: Application): void;
+    }
+}
+
+export class Application extends Mali {
+    public settings: any;
+    public context: any;
+    public scenes: { [key: string]: any } = {};
+    public services: { [key: string]: any } = {};
+    constructor(
+        path: any,
+        name?: string | ReadonlyArray<string>,
+        options?: any
+    ) {
+        super(path, name, options);
+        this.settings = _.merge({}, config);
+    }
+
+    get(name: string) {
+        return this.settings[name];
+    }
+
+    set(name: string, value: any) {
+        this.settings[name] = value;
+        return this;
+    }
+
+    disable(name: string) {
+        this.settings[name] = false;
+        return this;
+    }
+
+    disabled(name: string) {
+        return !this.settings[name];
+    }
+
+    enable(name: string) {
+        this.settings[name] = true;
+        return this;
+    }
+
+    enabled(name: string) {
+        return !!this.settings[name];
+    }
+
+    configure(fn: mikudos.ConfigFunc): Application {
+        fn.call(this, this);
+
+        return this;
+    }
+}
